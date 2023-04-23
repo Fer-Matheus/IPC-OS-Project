e81d5fbe1b058677727ded7c256d2751ed2c648e
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2023-04-23 11:20:30 -0300

    Add UNIX operating system support

--- ProcessB/Program.cs
+++ ProcessB/Program.cs
@@ -2,36 +2,83 @@
 using System.Text;
 using ProcessB;
 
-using (var file = new FileStream("/home/matheus/TEMP/sharedMemory", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
-using (var shm = MemoryMappedFile.CreateFromFile(file, null, 2048, MemoryMappedFileAccess.ReadWrite, HandleInheritability.None, true))
+if (!OperatingSystem.IsWindows())
 {
-    while (true)
+
+    string path = "/home/matheus/workspace/OS-Project/TEMP/";
+
+    using (var file = new FileStream(path + "SharedMemory", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+    using (var shm = MemoryMappedFile.CreateFromFile(file, null, 2048, MemoryMappedFileAccess.ReadWrite, HandleInheritability.None, true))
     {
-        if (OwnerMutex.WaitOne("/home/matheus/TEMP/mutex"))
+        while (true)
         {
-            using (var stream = shm.CreateViewStream())
+            if (OwnerMutex.WaitOne(path + "Mutex"))
             {
-                StreamReader reader = new(stream);
-                var received = reader.ReadToEnd();
-                if (received != null)
+                using (var stream = shm.CreateViewStream())
                 {
-                    Console.WriteLine($"Message received: {received}");
-                    stream.Write(Encoding.ASCII.GetBytes(""));
+                    StreamReader reader = new(stream);
+                    var received = reader.ReadToEnd();
+                    if (received != null)
+                    {
+                        Console.WriteLine($"Message received: {received}");
+                        stream.Write(Encoding.ASCII.GetBytes(""));
+                    }
+                }
+                using (var stream = shm.CreateViewStream())
+                {
+                    Console.WriteLine("Enter a message to processA:\n (enter 0 to exit)");
+                    var message = Console.ReadLine();
+                    if (message == "0") break;
+                    stream.Write(Encoding.UTF8.GetBytes(message));
+                    OwnerMutex.Release(path + "Mutex");
                 }
             }
-            using (var stream = shm.CreateViewStream())
-            {
-                Console.WriteLine("Enter a message to processA:\n (enter 0 to exit)");
-                var message = Console.ReadLine();
-                if (message == "0") break;
-                stream.Write(Encoding.UTF8.GetBytes(message));
-                OwnerMutex.Release("/home/matheus/TEMP/mutex");
-            }
+            Thread.Sleep(2000);
         }
-        Thread.Sleep(2000);
+        shm.Dispose();
+        file.Dispose();
+        File.Delete(path + "Mutex");
+        File.Delete(path + "SharedMemory");
     }
-    shm.Dispose();
-    file.Dispose();
-    File.Delete("/home/matheus/TEMP/mutex");
-    File.Delete("/home/matheus/TEMP/sharedMemory");
+}
+else
+{
+    try
+    {
+        using (var shm = MemoryMappedFile.CreateOrOpen("_SharedMemory", 2048))
+        {
+            Mutex mutex = new(false, "Mutex");
+            while (true)
+            {
+                if (mutex.WaitOne())
+                {
+                    using (var stream = shm.CreateViewStream())
+                    {
+                        StreamReader reader = new(stream);
+                        var received = reader.ReadToEnd();
+                        if (received != null)
+                        {
+                            Console.WriteLine($"Message received: {received}");
+                            stream.Write(Encoding.ASCII.GetBytes(""));
+                        }
+                    }
+                    using (var stream = shm.CreateViewStream())
+                    {
+                        Console.WriteLine("Enter a message to processA:\n (enter 0 to exit)");
+                        var message = Console.ReadLine();
+                        if (message == "0") break;
+                        stream.Write(Encoding.UTF8.GetBytes(message));
+                        mutex.ReleaseMutex();
+                    }
+                }
+                Thread.Sleep(2000);
+            }
+            mutex.Dispose();
+            shm.Dispose();
+        }
+    }
+    catch (System.Exception)
+    {
+        Console.WriteLine("The connection have been closed.");
+    }
 }
